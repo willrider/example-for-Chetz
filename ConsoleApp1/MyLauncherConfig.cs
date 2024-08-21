0d7b11eba9e330118cba18d8f83e35543e4738cc
Kevin Deenoo <kevindeenoo@paypoint.com>
2024-08-21 16:58:40 +0100

    quick and dirty just how we like it

---
+++ ConsoleApp1/MyLauncherConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+
+namespace ConsoleApp1;
+
+public class MyLauncherConfig
+{
+    public string CurrentProfile { get; set; } = "MHWorld";
+    public Profile[] Profiles { get; set; } = new Profile[1]
+    {
+        new Profile()
+        {
+            Id = 1,
+            Name = "MHWorld",
+            KeysDefinition = new Dictionary<string, string>
+            {
+                { "1", "C:\\apps\\1.exe" },
+                { "2", "C:\\apps\\2.exe" },
+                { "3", "C:\\apps\\3.exe" },
+                { "4", "C:\\apps\\4.exe" },
+                { "5", "C:\\apps\\5.exe" }
+            }
+        },
+    };
+}
+
+public class Profile
+{
+    public string Name { get; set; }
+    public long Id { get; set; }
+    public Dictionary<string, string> KeysDefinition { get; set; }
+}
+
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,25 @@
-﻿namespace ConsoleApp1;
+﻿using System.Text.Json;
+
+namespace ConsoleApp1;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        if (File.Exists("config.json"))
+        {
+            Console.WriteLine($"Config exists, loading...");
+            var deserializedJson = JsonSerializer.Deserialize<MyLauncherConfig>(File.ReadAllText("config.json"));
+            Console.WriteLine($"Current Profile: {deserializedJson.CurrentProfile} \n\r" +
+                              $"ID: {deserializedJson.Profiles.Where(x => x.Name == deserializedJson.CurrentProfile).First().Id}");
+        }
+        else
+        {
+            var myconfig = new MyLauncherConfig();
+            var serializedJson = JsonSerializer.Serialize<MyLauncherConfig>(myconfig);
+            File.WriteAllText("config.json", serializedJson);
+            Console.WriteLine("Config was saved!");
+        }
     }
 }
